558ad396238b4d96b0f58d96cabbce21f9b619ee
aakarkun <aakar.kun@hotmail.com>
2023-08-10 17:36:35 +1000

    Work experience added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -31,7 +31,7 @@ const ServiceCard = ({ index, title, icon }) => {
 const About = () => {
   return (
     <>
-      <motion.div variants={textVariant}>
+      <motion.div variants={textVariant()}>
         <p className={styles.sectionSubText}>Introduction</p>
         <h2 className={styles.sectionHeadText}>Overview.</h2>
       </motion.div>

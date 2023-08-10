a4585b83798722887380283ff08324e5399c8007
aakarkun <aakar.kun@hotmail.com>
2023-08-10 12:56:00 +1000

    Hero section update with Hello content

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section className={`relative w-full h-screen mx-auto`}>
+      <div
+        className={`absolute inset-0 top-[120px]
+          max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+      >
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+        </div>
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi, I'm <span className="text-[#915eff]">Kush</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 tex-white-100`}>
+            Developer | Illustrator | Designer | Blockchain | UI/UX
+          </p>
+        </div>
+      </div>
 
-export default Hero
+      <ComputersCanvas />
+    </section>
+  );
+};
+
+export default Hero;
